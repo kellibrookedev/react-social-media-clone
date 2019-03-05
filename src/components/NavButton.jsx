b98fe1cc5fa1755eca53e1f465c95d8e63eac2fc
Kelli Brooke <kellibrookedev@gmail.com>
2019-03-04 16:34:16 -0800

    added hot module reloader

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function NavButton(props) {
   return (
@@ -7,5 +8,9 @@ function NavButton(props) {
     </div>
   );
 }
+
+NavButton.propTypes = {
+  buttonName: PropTypes.string
+};
 
 export default NavButton;
